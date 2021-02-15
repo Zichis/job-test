0ca01622e85bc6f46f3bc612de927a93fbe220e3
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-15 21:58:03 +0100

    Added StoreModule to imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { paymentReducer } from './reducers/payment.reducer';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      payment: paymentReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
