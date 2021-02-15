924788882e84c500008d8af7b706f471992c6af7
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-15 21:56:07 +0100

    Created payment actions.

---
+++ src/app/actions/payment.actions.ts
@@ -0,0 +1,12 @@
+import { Action } from '@ngrx/store';
+import { Payment } from '../models/payment.model';
+
+export const ADD_PAYMENT = '[PAYMENT] Add';
+
+export class AddPayment implements Action {
+    readonly type = ADD_PAYMENT;
+
+    constructor(public payload: Payment) {}
+}
+
+export type Actions = AddPayment;
