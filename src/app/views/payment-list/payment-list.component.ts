3cb56189aafb6932948640282f20b2f4f65c7cac
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 14:54:52 +0100

    Added new dispatch.

--- src/app/views/payment-list/payment-list.component.ts
+++ src/app/views/payment-list/payment-list.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Payment } from '../../models/payment.model';
 import { AppState } from '../../app.state';
+import * as PaymentActions from '../../actions/payment.actions';
 
 @Component({
   selector: 'app-payment-list',
@@ -16,7 +17,9 @@ export class PaymentListComponent implements OnInit {
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-    this.payments = this.store.select(store => store.payment);
+    this.payments = this.store.select(store => store.payment.list);
+
+    this.store.dispatch(new PaymentActions.LoadPayment());
   }
 
 }
