4e7b34f8e39dbee7d67515fdece3344a1b00d6b8
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 01:35:19 +0100

    Displayed list of payments.

--- src/app/views/payment-list/payment-list.component.ts
+++ src/app/views/payment-list/payment-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Payment } from '../../models/payment.model';
+import { AppState } from '../../app.state';
 
 @Component({
   selector: 'app-payment-list',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-list.component.scss']
 })
 export class PaymentListComponent implements OnInit {
+  
+  payments: Observable<Payment[]>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.payments = this.store.select(store => store.payment);
   }
 
 }
