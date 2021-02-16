ea0fa02e138fc111989e06dab82aa274de198053
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 14:37:42 +0100

    Updated payment reducer Added payment state to replace payment model.

--- src/app/reducers/payment.reducer.ts
+++ src/app/reducers/payment.reducer.ts
@@ -2,18 +2,54 @@ import { Action } from '@ngrx/store';
 import { Payment } from '../models/payment.model';
 import * as PaymentActions from '../actions/payment.actions';
 
-const initialState: Payment = {
-    cardNumber: '98765',
-    cardHolder: 'John Doe',
-    expirationDate: new Date(),
-    securityCode: '101',
-    amount: 500
+export interface PaymentState {
+    list: Payment[],
+    loading: boolean,
+    error: Error
 }
 
-export function paymentReducer(state: Payment[] = [initialState], action: PaymentActions.Actions) {
+const initialState: PaymentState = {
+    list: [],
+    loading: false,
+    error: undefined
+}
+
+export function paymentReducer(state: PaymentState = initialState, action: PaymentActions.Actions) {
     switch (action.type) {
+        case PaymentActions.LOAD_PAYMENT:
+            return {
+                ...state,
+                loading: true
+            };
+        case PaymentActions.LOAD_PAYMENT_SUCCESS:
+            return {
+                ...state,
+                list: action.payload,
+                loading: false
+            };
+        case PaymentActions.LOAD_PAYMENT_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
+            };
         case PaymentActions.ADD_PAYMENT:
-            return [...state, action.payload];
+            return {
+                ...state,
+                loading: true
+            };
+        case PaymentActions.ADD_PAYMENT_SUCCESS:
+            return {
+                ...state,
+                list: [...state.list, action.payload],
+                loading: false
+            };
+        case PaymentActions.ADD_PAYMENT_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
+            };
         default:
             return state;
     }
