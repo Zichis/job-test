fcb166caa2f3484909f20b1bdcfe6799bf099ca2
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 14:52:55 +0100

    Add id to form.

--- src/app/views/payment-add/payment-add.component.ts
+++ src/app/views/payment-add/payment-add.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.state';
 import * as PaymentActions from '../../actions/payment.actions';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-payment-add',
@@ -12,6 +13,7 @@ import * as PaymentActions from '../../actions/payment.actions';
 export class PaymentAddComponent implements OnInit {
 
   paymentAddForm = this.fb.group({
+    id: [uuid()],
     cardNumber: [''],
     cardHolder: [''],
     expirationDate: [''],
