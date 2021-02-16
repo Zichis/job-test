060878aa1f5bc161f62a1da40d30deea72d691a0
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 10:05:27 +0100

    Added methods to service.

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Payment } from '../models/payment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  private PAYMENT_URL = "http://localhost:3000/payment";
+
+  constructor(private http: HttpClient) { }
+
+  getPayments() {
+    return this.http.get<Payment[]>(this.PAYMENT_URL);
+  }
+
+  addPayment(payment: Payment) {
+    return this.http.post(this.PAYMENT_URL, payment);
+  }
 }
