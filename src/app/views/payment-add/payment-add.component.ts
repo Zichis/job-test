0fd2f8b350dd9d4e04c631701a894f160c1328ba
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 02:30:42 +0100

    Added functionality to add payment.

--- src/app/views/payment-add/payment-add.component.ts
+++ src/app/views/payment-add/payment-add.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.state';
+import * as PaymentActions from '../../actions/payment.actions';
 
 @Component({
   selector: 'app-payment-add',
@@ -16,13 +19,14 @@ export class PaymentAddComponent implements OnInit {
     amount: ['']
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private store: Store<AppState>) { }
 
   ngOnInit(): void {
   }
 
   onAddPayment() {
-    //
+    console.log(this.paymentAddForm.value);
+    this.store.dispatch(new PaymentActions.AddPayment(this.paymentAddForm.value));
   }
 
 }
