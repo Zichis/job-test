004deaab1de336657696edb450b6b366943b790e
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 19:15:23 +0100

    Validator added to form.

--- src/app/views/payment-add/payment-add.component.ts
+++ src/app/views/payment-add/payment-add.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.state';
 import * as PaymentActions from '../../actions/payment.actions';
 import { v4 as uuid } from 'uuid';
+import { ccvValidator, dateValidator } from '../../validators/payment.validators';
 
 @Component({
   selector: 'app-payment-add',
@@ -14,11 +15,11 @@ export class PaymentAddComponent implements OnInit {
 
   paymentAddForm = this.fb.group({
     id: [uuid()],
-    cardNumber: [''],
-    cardHolder: [''],
-    expirationDate: [''],
-    securityCode: [''],
-    amount: ['']
+    cardNumber: ['', [Validators.required]],
+    cardHolder: ['', [Validators.required]],
+    expirationDate: ['', [Validators.required, dateValidator]],
+    securityCode: ['', [ccvValidator]],
+    amount: ['', [Validators.required]]
   });
 
   constructor(private fb: FormBuilder, private store: Store<AppState>) { }
@@ -26,9 +27,21 @@ export class PaymentAddComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get cardNumber(){ return this.paymentAddForm.get('cardNumber'); }
+
+  get cardHolder(){ return this.paymentAddForm.get('cardHolder'); }
+
+  get expirationDate(){ return this.paymentAddForm.get('expirationDate'); }
+
+  get securityCode(){ return this.paymentAddForm.get('securityCode'); }
+
+  get amount(){ return this.paymentAddForm.get('amount'); }
+
   onAddPayment() {
+    console.log(this.paymentAddForm.controls.securityCode);
+    console.log(this.paymentAddForm.get('securityCode').value.length);
     console.log(this.paymentAddForm.value);
-    this.store.dispatch(new PaymentActions.AddPayment(this.paymentAddForm.value));
+    //this.store.dispatch(new PaymentActions.AddPayment(this.paymentAddForm.value));
   }
 
 }
