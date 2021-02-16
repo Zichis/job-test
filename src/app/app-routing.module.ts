132252299fc4808b7dcd43856cc9e608a909eb74
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 01:26:23 +0100

    Added routes for adding and listing payments.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaymentAddComponent } from './views/payment-add/payment-add.component';
+import { PaymentListComponent } from './views/payment-list/payment-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PaymentListComponent },
+  { path: 'payment', component: PaymentAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
