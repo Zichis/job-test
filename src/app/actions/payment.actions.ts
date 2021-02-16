1582700e25139fd1806ea7017565f881a1f5b11b
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 14:32:25 +0100

    Updated payment actions Added success and failure states.

--- src/app/actions/payment.actions.ts
+++ src/app/actions/payment.actions.ts
@@ -1,7 +1,27 @@
 import { Action } from '@ngrx/store';
 import { Payment } from '../models/payment.model';
 
-export const ADD_PAYMENT = '[PAYMENT] Add';
+export const ADD_PAYMENT = '[PAYMENT] Add Payment';
+export const ADD_PAYMENT_SUCCESS = '[PAYMENT] Add Payment Success';
+export const ADD_PAYMENT_FAILURE = '[PAYMENT] Add Payment Failure';
+export const LOAD_PAYMENT = '[PAYMENT] Load Payment';
+export const LOAD_PAYMENT_SUCCESS = '[PAYMENT] Load Payment Success';
+export const LOAD_PAYMENT_FAILURE = '[PAYMENT] Load Payment Failure';
+
+export class LoadPayment implements Action {
+    readonly type = LOAD_PAYMENT;
+}
+export class LoadPaymentSuccess implements Action {
+    readonly type = LOAD_PAYMENT_SUCCESS;
+
+    constructor(public payload: Payment[]) {}
+
+}
+export class LoadPaymentFailure implements Action {
+    readonly type = LOAD_PAYMENT_FAILURE
+
+    constructor(public payload: Error) {}
+}
 
 export class AddPayment implements Action {
     readonly type = ADD_PAYMENT;
@@ -9,4 +29,21 @@ export class AddPayment implements Action {
     constructor(public payload: Payment) {}
 }
 
-export type Actions = AddPayment;
+export class AddPaymentSuccess implements Action {
+    readonly type = ADD_PAYMENT_SUCCESS;
+
+    constructor(public payload: Payment) {}
+}
+
+export class AddPaymentFailure implements Action {
+    readonly type = ADD_PAYMENT_FAILURE;
+
+    constructor(public payload: Error) {}
+}
+
+export type Actions = AddPayment |
+    AddPaymentSuccess |
+    AddPaymentFailure |
+    LoadPayment |
+    LoadPaymentSuccess |
+    LoadPaymentFailure
