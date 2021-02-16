c12acbf2f69bcc800308c083078a7f88efdf992f
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-16 19:14:15 +0100

    Custom validator fuctions added.

---
+++ src/app/validators/payment.validators.ts
@@ -0,0 +1,32 @@
+import { AbstractControl } from "@angular/forms";
+
+export function ccvValidator(control: AbstractControl) {
+    const ccvLength = control.value.length;
+    if ((ccvLength > 0 && ccvLength < 3) || ccvLength > 3) {
+        return {
+            isError: true
+        }
+    }
+
+    return null;
+}
+
+export function dateValidator(control: AbstractControl) {
+    let dateRegex = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
+    if (!(dateRegex.test(control.value))) {
+        return {
+            isError: true
+        }
+    }
+
+    let currentDate = new Date();
+    let selectedDate = new Date(control.value);
+
+    if (currentDate > selectedDate) {
+        return {
+            isError: true
+        }
+    }
+
+    return null;
+}
